7d3fc20de132c1c3f212d910a9ab5820baa2f778
Fernando Rodríguez Pérez <fernando.rodriguez.perez12@gmail.com>
2026-09-09 16:48:27 -0400

    Adding Restaurante entity

---
+++ app/src/main/java/com/ferdev/whattoeatapp/data/Restaurante.kt
@@ -0,0 +1,13 @@
+package com.ferdev.whattoeatapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "RESTAURANT")
+data class Restaurante(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val address: String,
+    val phone: String
+)
