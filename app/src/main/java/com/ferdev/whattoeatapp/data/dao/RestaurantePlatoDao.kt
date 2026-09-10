0d4345bfe9b379f95e0850e2f1481886bd4c4d3a
Fernando Rodríguez Pérez <fernando.rodriguez.perez12@gmail.com>
2026-09-10 17:36:14 -0400

    Adding RestaurantePlato DataAccessObj

---
+++ app/src/main/java/com/ferdev/whattoeatapp/data/dao/RestaurantePlatoDao.kt
@@ -0,0 +1,15 @@
+package com.ferdev.whattoeatapp.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.ferdev.whattoeatapp.data.RestaurantePlato
+
+@Dao
+interface RestaurantePlatoDao {
+    @Insert
+    suspend fun insert(restaurantePlato: RestaurantePlato)
+
+    @Query("SELECT * FROM RESTAURANT_PLATO WHERE restaurant_id = :restaurantId")
+    suspend fun getByRestaurant(restaurantId: Int): List<RestaurantePlato>
+}
