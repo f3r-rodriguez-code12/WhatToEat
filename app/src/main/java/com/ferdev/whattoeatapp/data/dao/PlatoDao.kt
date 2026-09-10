9d2e4c2a53fd1e8bf31fa24e7cce060b61f010d9
Fernando Rodríguez Pérez <fernando.rodriguez.perez12@gmail.com>
2026-09-10 17:21:25 -0400

    Adding Plato DataAccessObj

---
+++ app/src/main/java/com/ferdev/whattoeatapp/data/dao/PlatoDao.kt
@@ -0,0 +1,15 @@
+package com.ferdev.whattoeatapp.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.ferdev.whattoeatapp.data.Plato
+
+@Dao
+interface PlatoDao {
+    @Insert
+    suspend fun insert(plato: Plato)
+
+    @Query("SELECT * FROM PLATO")
+    suspend fun getAll(): List<Plato>
+}
