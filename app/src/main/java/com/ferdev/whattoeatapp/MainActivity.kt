6993198a8e945e3c13f7065cd4571ded095e2566
Fernando Rodríguez Pérez <fernando.rodriguez.perez12@gmail.com>
2026-09-16 22:33:27 -0400

    Calling DB instance and Seeding data

--- app/src/main/java/com/ferdev/whattoeatapp/MainActivity.kt
+++ app/src/main/java/com/ferdev/whattoeatapp/MainActivity.kt
@@ -4,44 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.ferdev.whattoeatapp.data.AppDatabase
+import com.ferdev.whattoeatapp.data.DataSeeder
 import com.ferdev.whattoeatapp.ui.theme.WhatToEatAppTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var database: AppDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        database = AppDatabase.getInstance(this)
+
+        val prefs = getSharedPreferences("app_prefs", MODE_PRIVATE)
+        val isSeeded = prefs.getBoolean("data_seeded", false)
+        if (!isSeeded) {
+            DataSeeder(database).seedData()
+            prefs.edit().putBoolean("data_seeded", true).apply()
+        }
+
         setContent {
             WhatToEatAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                // RegistroScreen(database = database)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WhatToEatAppTheme {
-        Greeting("Android")
     }
 }
